36589564b4b1a8597db32fccd60cea6cff7ddd81
Evin Dunn <evindunn14@gmail.com>
2019-06-20 21:24:50 -0700

    Starts header implementation

---
+++ header.bak.php
@@ -0,0 +1,103 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css' media='all' />
+<script src="<?php echo $clientRoot; ?>/js/jquery-ui-1.12.1/external/jquery/jquery.js"></script>
+<script src="<?php echo $clientRoot; ?>/js/jquery-ui-1.12.1/jquery-ui.js"></script>
+<script src="<?php echo $clientRoot; ?>/js/superfish.min.js"></script>
+<script src="<?php echo $clientRoot; ?>/js/menu.js"></script>
+<link href="<?php echo $clientRoot; ?>/css/component.css" type="text/css" rel="stylesheet" />
+<div class="container" id="maintable">
+    <div class="header-wrapper clearfix">
+        <div class="top-menu-container">
+            <div class="top-menu">
+                <ul>
+                    <li><a href="<?php echo $clientRoot; ?>/pages/contact.php">Contact Info</a></li>
+                    <li><a href="<?php echo $clientRoot; ?>/pages/donate.php">Donate</a></li>
+	                <?php
+	                if($userDisplayName){
+		                ?>
+                        <li><a href="">Welcome <?php echo $userDisplayName; ?>!</a></li>
+                        <li><a href="<?php echo $clientRoot; ?>/profile/viewprofile.php">My Profile</a></li>
+                        <li><a href="<?php echo $clientRoot; ?>/profile/index.php?submit=logout">Logout</a></li>
+		                <?php
+	                }
+	                else{
+		                ?>
+                        <li><a href="<?php echo $clientRoot."/profile/index.php?refurl=".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']; ?>">Log In</a></li>
+		                <?php
+	                }
+	                ?>
+                </ul>
+            </div>
+        </div>
+        <div class="main-header">
+            <div class="header-logo">
+                <a href="<?php echo $clientRoot; ?>/index.php"><img src="<?php echo $clientRoot; ?>/images/layout/new-logo.png" alt="Oregon Flora"></a>
+            </div><!-- .logo -->
+            <div class="search-wrapper">
+		        <?php
+		        //---------------------------QUICK SEARCH SETTINGS---------------------------------------
+		        //Title text that will appear.
+		        $searchText = 'Plant Taxon Search';
+		        //Placeholder text that will appear.
+		        $placeholderText = "Enter taxon name";
+
+		        //Text that will appear on search button.
+		        $buttonText = '<i class="fa fa-search"></i>';
+
+		        //---------------------------DO NOT CHANGE BELOW HERE-----------------------------
+		        include_once($SERVER_ROOT.'/classes/PluginsManager.php');
+		        $pluginManager = new PluginsManager();
+		        $quicksearch = $pluginManager->createQuickSearch($buttonText,$searchText,$placeholderText);
+		        echo $quicksearch;
+		        ?>
+                <!--  common name search temporarily added here for testing.-->
+                <form id="common-quick-search" action="<?php echo $CLIENT_ROOT ?>/taxa/common.php">
+                    <div class="common-search-wrapper">
+                        <div class="input-label">Common name search</div>
+                        <div class="input-wrapper">
+                            <input type="text" name="common" id="quicksearchcommon" placeholder="Enter common name here" a />
+                            <button id="garden-common-search-but" type="submit" ><i class="fa fa-search"></i></button>
+                        </div>
+                </form>
+            </div><!-- .search-wrapper -->
+            <nav class="main-navigation" id="site-navigation">
+                <button class="menu-toggle" id="menu-button" aria-controls="primary-menu" aria-expanded="false">&#8681; Menu</button>
+                <ul class="nav-menu">
+                    <li class="menu-item-has-children"><a href="#">Explore Our Site</a>
+                        <ul>
+                            <li><a href="<?php echo $clientRoot; ?>/spatial/index.php">Mapping</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=key">Interactive Key</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/projects/index.php">Plant Inventories</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/collections/harvestparams.php?db[]=5,8,10,7,238,239,240,241">OSU Herbarium</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/garden/index.php">Gardening with Natives</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/imagelib/search.php">Image Search</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/taxa/admin/taxonomydisplay.php">Taxonomic Tree</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children"><a href="#">Resources</a>
+                        <ul>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/whats-new.php">What's New</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/newsletters/index.php">Archived Newsletter</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/links.php">Links</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children"><a href="#">About</a>
+                        <ul>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/mission.php">Mission and History</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/contact.php">Contact Info</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/project-participants.php">Project Participants</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children"><a href="#">Support</a>
+                        <ul>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/donate.php">Donate</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/volunteer.php">Volunteer</a></li>
+                            <li><a href="<?php echo $clientRoot; ?>/pages/merchandise.php">Merchandise</a></li>
+                        </ul>
+                    </li>
+                </ul><!-- .nav -->
+            </nav><!-- .main-nav -->
+        </div><!--.main-header -->
+    </div><!-- .header-wrapper -->
+    <div class="content-wrapper" id="site-content">
--- header.php
+++ header.php
@@ -1,4 +1,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<?php
+// TOOD: Remove, dev only
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+
+<!-- Compat stuff -->
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
 <link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css' media='all' />
 <script src="<?php echo $clientRoot; ?>/js/jquery-ui-1.12.1/external/jquery/jquery.js"></script>
@@ -6,98 +15,93 @@
 <script src="<?php echo $clientRoot; ?>/js/superfish.min.js"></script>
 <script src="<?php echo $clientRoot; ?>/js/menu.js"></script>
 <link href="<?php echo $clientRoot; ?>/css/component.css" type="text/css" rel="stylesheet" />
-<div class="container" id="maintable">
-    <div class="header-wrapper clearfix">
-        <div class="top-menu-container">
-            <div class="top-menu">
-                <ul>
-                    <li><a href="<?php echo $clientRoot; ?>/pages/contact.php">Contact Info</a></li>
-                    <li><a href="<?php echo $clientRoot; ?>/pages/donate.php">Donate</a></li>
-	                <?php
-	                if($userDisplayName){
-		                ?>
-                        <li><a href="">Welcome <?php echo $userDisplayName; ?>!</a></li>
-                        <li><a href="<?php echo $clientRoot; ?>/profile/viewprofile.php">My Profile</a></li>
-                        <li><a href="<?php echo $clientRoot; ?>/profile/index.php?submit=logout">Logout</a></li>
-		                <?php
-	                }
-	                else{
-		                ?>
-                        <li><a href="<?php echo $clientRoot."/profile/index.php?refurl=".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']; ?>">Log In</a></li>
-		                <?php
-	                }
-	                ?>
-                </ul>
-            </div>
-        </div>
-        <div class="main-header">
-            <div class="header-logo">
-                <a href="<?php echo $clientRoot; ?>/index.php"><img src="<?php echo $clientRoot; ?>/images/layout/new-logo.png" alt="Oregon Flora"></a>
-            </div><!-- .logo -->
-            <div class="search-wrapper">
-		        <?php
-		        //---------------------------QUICK SEARCH SETTINGS---------------------------------------
-		        //Title text that will appear.
-		        $searchText = 'Plant Taxon Search';
-		        //Placeholder text that will appear.
-		        $placeholderText = "Enter taxon name";
 
-		        //Text that will appear on search button.
-		        $buttonText = '<i class="fa fa-search"></i>';
+<!-- Bootstrap Deps -->
+<script
+  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous">
+</script>
+<script
+  src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+  crossorigin="anonymous">
+</script>
 
-		        //---------------------------DO NOT CHANGE BELOW HERE-----------------------------
-		        include_once($SERVER_ROOT.'/classes/PluginsManager.php');
-		        $pluginManager = new PluginsManager();
-		        $quicksearch = $pluginManager->createQuickSearch($buttonText,$searchText,$placeholderText);
-		        echo $quicksearch;
-		        ?>
-                <!--  common name search temporarily added here for testing.-->
-                <form id="common-quick-search" action="<?php echo $CLIENT_ROOT ?>/taxa/common.php">
-                    <div class="common-search-wrapper">
-                        <div class="input-label">Common name search</div>
-                        <div class="input-wrapper">
-                            <input type="text" name="common" id="quicksearchcommon" placeholder="Enter common name here" a />
-                            <button id="garden-common-search-but" type="submit" ><i class="fa fa-search"></i></button>
-                        </div>
-                </form>
-            </div><!-- .search-wrapper -->
-            <nav class="main-navigation" id="site-navigation">
-                <button class="menu-toggle" id="menu-button" aria-controls="primary-menu" aria-expanded="false">&#8681; Menu</button>
-                <ul class="nav-menu">
-                    <li class="menu-item-has-children"><a href="#">Explore Our Site</a>
-                        <ul>
-                            <li><a href="<?php echo $clientRoot; ?>/spatial/index.php">Mapping</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=key">Interactive Key</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/projects/index.php">Plant Inventories</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/collections/harvestparams.php?db[]=5,8,10,7,238,239,240,241">OSU Herbarium</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/garden/index.php">Gardening with Natives</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/imagelib/search.php">Image Search</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/taxa/admin/taxonomydisplay.php">Taxonomic Tree</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu-item-has-children"><a href="#">Resources</a>
-                        <ul>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/whats-new.php">What's New</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/newsletters/index.php">Archived Newsletter</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/links.php">Links</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu-item-has-children"><a href="#">About</a>
-                        <ul>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/mission.php">Mission and History</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/contact.php">Contact Info</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/project-participants.php">Project Participants</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu-item-has-children"><a href="#">Support</a>
-                        <ul>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/donate.php">Donate</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/volunteer.php">Volunteer</a></li>
-                            <li><a href="<?php echo $clientRoot; ?>/pages/merchandise.php">Merchandise</a></li>
-                        </ul>
-                    </li>
-                </ul><!-- .nav -->
-            </nav><!-- .main-nav -->
-        </div><!--.main-header -->
-    </div><!-- .header-wrapper -->
-    <div class="content-wrapper" id="site-content">
+<!-- Bootstrap -->
+<link
+  rel="stylesheet"
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous">
+<script
+  src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+  crossorigin="anonymous">
+</script>
+
+<style>
+  #navbar-container {
+    position: relative;
+    padding: 0;
+    margin: 0;
+  }
+
+  #navbar-background {
+    position: absolute;
+    width: 100%;
+    height: 75%;
+    z-index: -1;
+
+    background-image: url(<?php echo $clientRoot; ?>/images/layout/whois-bg.jpg);
+    background-size: 100% auto;
+  }
+
+  .dropdown:hover .dropdown-menu {
+    display: block;
+  }
+
+  .nav-link.dropdown-toggle {
+    text-transform: uppercase;
+    color: white !important;
+  }
+
+  /* Get rid of caret */
+  .nav-link.dropdown-toggle::after {
+    display: none;
+  }
+</style>
+
+<!-- Navbar -->
+<div id="navbar-container">
+  <div id="navbar-background" class="shadow"></div>
+  <nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="<?php echo $clientRoot; ?>/index.php">
+      <img src="<?php echo $clientRoot; ?>/images/layout/new-logo.png" alt="Oregon Flora">
+    </a>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto ml-2">
+        <!-- Explore our Site -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="explore">
+            Explore Our Site
+          </a>
+          <div class="dropdown-menu" aria-labelledby="explore">
+            <a class="dropdown-item" href="<?php echo $clientRoot; ?>/spatial/index.php">Mapping</a>
+            <a class="dropdown-item" href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=key">Interactive Key</a>
+            <a class="dropdown-item" href="<?php echo $clientRoot; ?>/projects/index.php">Plant Inventories</a>
+            <a class="dropdown-item" href="<?php echo $clientRoot; ?>/collections/harvestparams.php?db[]=5,8,10,7,238,239,240,241">OSU Herbarium</a>
+            <a class="dropdown-item" href="<?php echo $clientRoot; ?>/garden/index.php">Gardening with Natives</a>
+            <a class="dropdown-item" href="<?php echo $clientRoot; ?>/imagelib/search.php">Image Search</a>
+            <a class="dropdown-item" href="<?php echo $clientRoot; ?>/taxa/admin/taxonomydisplay.php">Taxonomic Tree</a>
+          </div>
+        </li>
+
+
+      </ul>
+    </div>
+  </nav>
+</div>
+
+<div id="site-content">
